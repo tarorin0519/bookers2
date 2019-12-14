48e4e8c2b9dab120aad13482e547e4167ee5ff91
tarorin0519 <rin.ice.musica0519@gmail.com>
2019-12-14 21:13:16 +0900

    [Fix]5-9

--- spec/factories/books.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :book do
-    title { Faker::Lorem.characters(5) }
-    body { Faker::Lorem.characters(20) }
-    user
-  end
-end
--- spec/factories/users.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    name { Faker::Lorem.characters(10) }
-    email { Faker::Internet.email }
-    introduction { Faker::Lorem.characters(20) }
-    password { 'password' }
-    password_confirmation { 'password' }
-  end
-end
