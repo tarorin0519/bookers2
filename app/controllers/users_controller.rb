23523db0c5a3bdb9ca4a7a57b29731c4da632a2b
tarorin0519 <rin.ice.musica0519@gmail.com>
2019-12-14 20:59:35 +0900

    [Finish]5-9

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :correct_user, only: [:edit, :update]
+  def show
+  	@user = User.find(params[:id])
+  	@books = @user.books
+    @book = Book.new
+  end
+  def edit
+  	@user = User.find(params[:id])
+  end
+  def update
+  	@user = User.find(params[:id])
+  	if @user.update(user_params)
+  	   redirect_to user_path(@user.id)
+       flash[:notice] = "You have updated user successfully."
+    else
+      render :edit
+    end
+  end
+  def index
+  	@users = User.all
+    @book = Book.new
+
+  end
+  private
+    def user_params
+      params.require(:user).permit(:name, :introduction, :profile_image)
+    end
+    def correct_user
+      user = User.find(params[:id])
+      if current_user != user
+        redirect_to user_path(current_user)
+    end
+    end
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '82a63ca663d5553b110f89c022372518daf219bd8335864842c5e15b12db5499ab4756423ed6a3cb655dce45768aa01bb4ff74677e84a3468cd466eca0dfea11'
